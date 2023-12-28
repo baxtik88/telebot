1900e14a06d77baa23fe2f2a57123d89701da50d
baxtik <baxtik88@gmail.com>
2023-12-28 19:34:40 +0500

    final

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть web страницу',
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Открыть web страницу',
                                     web_app=WebAppInfo(url='https://baxtik88.github.io/telebot/index.html')))
     await message.answer('Здравствуйте уважаемый посетитель!', reply_markup=markup)
 
